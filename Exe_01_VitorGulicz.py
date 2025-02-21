1c98c456d3394598d2949f43aaed75fc67c1f5c8
VitorGulicz <vdmgulicz@gmail.com>
2025-02-21 18:54:25 -0300

    update

--- Exe_01_VitorGulicz.py
+++ Exe_01_VitorGulicz.py
@@ -1,7 +1,7 @@
 #Crie uma tupla contendo os nomes de cinco países e exiba toda a tupla. 
 #Peça ao usuário para inserir um dos países que foram mostrados a ele e, em seguida, exibir o número de índice (ou seja, posição na lista) desse item na tupla.
 paises=("Brasil","Venezuela","Argentina","Peru","Guatemala")
-pais=input("Digite um dos paises a seguir:{}".format(paises))
+pais=input("Digite um dos paises a seguir:{} ".format(paises))
 if pais in paises:
     i=paises.index(pais)
     print("A posição deste pais na lista é {} - Vitor Gulicz".format(i))
